b26b5ee96f5f92706b4fe58afd73768a628c3108
jin0736 <jin007736@gmail.com>
2023-11-20 19:50:34 +0900

    밴드, 인스타 뒤로가기 추가

--- lib/instagramList.dart
+++ lib/instagramList.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:elderly_people_sns/main.dart';
 
 class InstagramList extends StatelessWidget {
    InstagramList({super.key});
-  List<String> naverBandLearningList = [
+  List<String> instagramLearningList = [
     '스토리 올리기',
     '게시물 올리기',
     '팔로우 하기'];
@@ -30,50 +31,71 @@ class InstagramList extends StatelessWidget {
               )),
           backgroundColor: Colors.pinkAccent,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Stack(
             children: [
-              for (int i = 0; i < 3; i += 1)
-                InkWell(
-                  onTap: (){
-                    // Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => const NaverBandSignUp01()));
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    for (int i = 0; i < 3; i += 1)
+                      InkWell(
+                        onTap: (){
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) => const NaverBandSignUp01()));
+                        },
+                        customBorder: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Ink(
+                          width: 370,
+                          height: 88,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffffffff),
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.grey,
+                                offset: Offset(0.0, 4.0), //(x,y)
+                                blurRadius: 1.0,
+                              ),
+                            ],
+                          ),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              instagramLearningList[i],
+                              maxLines: 3,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 10.0,
+                left: 10.0,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainScreen()),
+                    );
                   },
-                  customBorder: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    'assets/images/facetalkImages/backButton.png',
+                    width: 70,
+                    height: 70,
                   ),
-                  child: Ink(
-                    width: 370,
-                    height: 88,
-                    decoration: BoxDecoration(
-                      color: const Color(0xffffffff),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(0.0, 4.0), //(x,y)
-                          blurRadius: 1.0,
-                        ),
-                      ],
-                    ),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        naverBandLearningList[i],
-                        maxLines: 3,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-            ],
-          ),
+                ),
+              ),
+            ]
         ),
       ),
     );
