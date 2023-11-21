27bef01ca78398644fdb582869790eaa433e489a
lilminkii <lol3410@naver.com>
2023-11-21 19:43:45 +0900

    bandexit2

--- lib/mingi/band_exit_01.dart
+++ lib/mingi/band_exit_01.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'band_exit_02.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Screen1());
 }
 
-class MyApp extends StatelessWidget {
+class Screen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
